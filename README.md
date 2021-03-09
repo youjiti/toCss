7b6ca50e229c2012c4b460092f94938400e8d090
youjiti <youjiti@youjiti-MacBook-Pro.local>
2021-03-09 16:16:53 +0800

    1.0

--- README.md
+++ README.md
@@ -1,65 +1,15 @@
 # tocss README
 
-This is the README for your extension "tocss". After writing up a brief description, we recommend including the following sections.
+const indexStyles1 = require('../../index1.less')<br/>
+const indexStyles2 = require('../../../index2.less')<br/>
+<br/>
+indexStyles1.astyle<br/>
+indexStyles2.bstyle<br/>
 
-## Features
+光标放在astyle或者bstyle上 按住command键在 点击左键 从js/ts文件跳转到less文件定义的classname处<br/>
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+>**支持文件引入多个less文件的跳转**
+<br/>
+>**支持&符号拼接而成的classname**
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,121 @@
 import * as vscode from 'vscode';
+import * as parser from '@babel/parser';
+import * as traverse from '@babel/traverse';
+import * as path from 'path';
+// import * as postcss from 'postcss';
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const postcss_less = require('postcss-less');
+
+function compileStylesName(code: string, name: string, line: Number) {
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["typescript", "jsx"]
+  });
+  let stylesName = '';
+  const visitor = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    Identifier(path: any) {
+      if (path?.node?.name === name && path?.node?.loc?.start?.line === line) {
+        stylesName = stylesName || path?.parent?.object?.name;
+        return;
+      }
+    }
+  };
+
+  traverse.default(ast, visitor);
+
+  return stylesName;
+}
+
+function compileRequirePath(code: string, name: string) {
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["typescript", "jsx"]
+  });
+  let requirePath = '';
+  const visitor = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    Identifier(path: any) {
+      if (path.node.name === 'require') {
+        const parent = path.findParent((paths: any) => paths.isVariableDeclarator());
+        if (parent?.node?.id?.name === name) {
+          requirePath = requirePath || (path?.parent?.arguments[0] || {})?.value;
+          return;
+        }
+      }
+    }
+  };
+
+  traverse.default(ast, visitor);
+
+  return requirePath;
+}
+
+let classNames: any = {};
+
+function compileCss(rules: any[] = []) {
+  if (rules.length === 0) { return; }
+  rules.forEach((rule: any) => {
+    rule.selector = rule.selector.substr((rule.f || '') ? 1 : 0);
+    if(rule.selector.includes(',')){
+      const arr = rule.selector.split(',');
+      classNames[(rule.f || '') + arr[0]] = rule.positionInside().line;
+      classNames[(rule.f || '') + arr[1].split('&')[1]] = rule.positionInside().line + 1;
+    }else{
+      classNames[(rule.f || '') + rule.selector] = rule.positionInside().line;
+    }
+    compileCss((rule.nodes || []).filter((node: any) => {
+      if ((node?.selector || '')[0] === '&') {
+        node.f = (rule.f || '') + rule.selector;
+        return true;
+      }
+    }));
+  });
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('tocss.helloWorld', () => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  let disposable_toCss = vscode.commands.registerCommand('tocss.toCss', async () => {
+    const editor = vscode.window.activeTextEditor;
+    const document = editor?.document;
+    const selection = editor?.selection;
+    const arr = document?.getText(selection).split('.')!;
+    const requirePath = compileRequirePath(document?.getText()!, arr[0]);
+    const uri = vscode.Uri.file(path.resolve(document?.fileName!, '../' + requirePath));
+    const cssdoc = (await vscode.workspace.openTextDocument(uri));
+    const root = postcss_less.parse(cssdoc.getText());
+    classNames = {};
+    compileCss((root?.nodes || []).filter((rule: any) => rule.type === 'rule'));
+    const index = (classNames['.' + arr[1]] || 1) - 1;
+    const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
+    vscode.window.showTextDocument(cssdoc, {
+      selection: range,
+    });
+  });
 
-		vscode.window.showInformationMessage('Hello World from toCss!');
-	});
+  let definition = vscode.languages.registerDefinitionProvider([
+    { language: 'javascript', scheme: 'file' },
+    { language: 'typescriptreact', scheme: 'file' }], {
+    async provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+      const name = document.getText(document.getWordRangeAtPosition(position));
+      const text = document.getText();
+      const stylesName = compileStylesName(text, name, position.line + 1);
+      const requirePath = compileRequirePath(text, stylesName);
+      const uri = vscode.Uri.file(path.resolve(document.fileName, '../' + requirePath));
+      const cssdoc = (await vscode.workspace.openTextDocument(uri));
+      const root = postcss_less.parse(cssdoc?.getText());
+      classNames = {};
+      compileCss((root?.nodes || []).filter((rule: any) => rule.type === 'rule'));
+      const index = (classNames['.' + name] || 1) - 1;
+      const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
+      return new vscode.Location((uri), range);
+    }
+  });
 
-	context.subscriptions.push(disposable); 
+  context.subscriptions.push(disposable_toCss);
+  context.subscriptions.push(definition);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
