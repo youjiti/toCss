996ccd04a7434741930577a99b6de4f13bcb4838
youjiti <youjiti@youjiti-MacBook-Pro.local>
2021-03-12 13:58:23 +0800

    2.0

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # tocss README
 
-const indexStyles1 = require('../../index1.less')<br/>
+import indexStyles1 from '../../index1.less'<br/>
 const indexStyles2 = require('../../../index2.less')<br/>
+
 <br/>
 indexStyles1.astyle<br/>
 indexStyles2.bstyle<br/>
@@ -9,7 +10,6 @@ indexStyles2.bstyle<br/>
 光标放在astyle或者bstyle上 按住command键在 点击左键 从js/ts文件跳转到less文件定义的classname处<br/>
 
 >**支持文件引入多个less文件的跳转**
-<br/>
 >**支持&符号拼接而成的classname**
 
-**Enjoy!**
+**性能优化!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,110 +1,62 @@
 import * as vscode from 'vscode';
-import * as parser from '@babel/parser';
-import * as traverse from '@babel/traverse';
 import * as path from 'path';
-// import * as postcss from 'postcss';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const postcss_less = require('postcss-less');
 
-function compileStylesName(code: string, name: string, line: Number) {
-  const ast = parser.parse(code, {
-    sourceType: "module",
-    plugins: ["typescript", "jsx"]
-  });
-  let stylesName = '';
-  const visitor = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Identifier(path: any) {
-      if (path?.node?.name === name && path?.node?.loc?.start?.line === line) {
-        stylesName = stylesName || path?.parent?.object?.name;
-        return;
-      }
-    }
-  };
+let classNames = '';
+let lines = 0;
 
-  traverse.default(ast, visitor);
-
-  return stylesName;
-}
-
-function compileRequirePath(code: string, name: string) {
-  const ast = parser.parse(code, {
-    sourceType: "module",
-    plugins: ["typescript", "jsx"]
-  });
-  let requirePath = '';
-  const visitor = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Identifier(path: any) {
-      if (path.node.name === 'require') {
-        const parent = path.findParent((paths: any) => paths.isVariableDeclarator());
-        if (parent?.node?.id?.name === name) {
-          requirePath = requirePath || (path?.parent?.arguments[0] || {})?.value;
+function compileCss(rules: any[] = []) {
+  if (rules.length === 0 || lines !== 0) { return; }
+  for (let index = 0; index < rules.length; index++) {
+    const rule = rules[index];
+    rule.selector = rule.selector.substr(rule.selector[0] === '&' ? 1 : 0);
+    if (rule.f || rule.selector[0] === '.') {
+      if (rule.selector.includes(',')) {
+        const arr = rule.selector.split(',');
+        let i = -1;
+        for (let j = 0; j < arr.length; j++) {
+          let rulej = arr[j];
+          if (rulej.includes('\n')) {
+            i++;
+          }
+          if (rulej.includes('&')) {
+            rulej = rulej.split('&')[1];
+          }
+          rulej = rulej.replace(/(^\s*)|(\s*$)/g, "");
+          if (classNames === ((rulej[0] === '.' ? '' : (rule.f || '')) + rulej)) {
+            lines = i + rule.positionInside().line;
+            return;
+          }
+        }
+      } else {
+        if ((rule.f || '') + rule.selector.replace(/(^\s*)|(\s*$)/g, "") === classNames) {
+          lines = rule.positionInside().line - 1;
           return;
         }
       }
     }
-  };
-
-  traverse.default(ast, visitor);
-
-  return requirePath;
-}
-
-let classNames: any = {};
-
-function compileCss(rules: any[] = []) {
-  if (rules.length === 0) { return; }
-  rules.forEach((rule: any) => {
-    rule.selector = rule.selector.substr(rule.selector[0] === '&' ? 1 : 0);
-    if(rule.f||rule.selector[0] === '.'){
-      if(rule.selector.includes(',')){
-        const arr = rule.selector.split(',');
-        classNames[(rule.f || '') + arr[0]] = rule.positionInside().line;
-        classNames[(rule.f || '') + arr[1].split('&')[1]] = rule.positionInside().line + 1;
-      }else{
-        classNames[(rule.f || '') + rule.selector] = rule.positionInside().line;
-      }
-    }
+    if (lines !== 0) { return; }
     compileCss((rule.nodes || []).filter((node: any) => {
       if ((node?.selector || '')[0] === '&') {
         node.f = rule.s ? '' : (rule.f || '') + rule.selector;
         return true;
-      } else if((node?.selector || '')[0] === '.'){
+      } else if ((node?.selector || '')[0] === '.') {
         node.f = '';
         return true;
-      } else if(node.type === 'rule'){
-        compileCss(node.nodes.filter((r:any)=>{
-          if(r.type === 'rule'){
+      } else if (node.type === 'rule') {
+        compileCss(node.nodes.filter((r: any) => {
+          if (r.type === 'rule') {
             r.s = true;
             return true;
           }
         }));
       }
     }));
-  });
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  let disposable_toCss = vscode.commands.registerCommand('tocss.toCss', async () => {
-    const editor = vscode.window.activeTextEditor;
-    const document = editor?.document;
-    const selection = editor?.selection;
-    const arr = document?.getText(selection).split('.')!;
-    const requirePath = compileRequirePath(document?.getText()!, arr[0]);
-    const uri = vscode.Uri.file(path.resolve(document?.fileName!, '../' + requirePath));
-    const cssdoc = (await vscode.workspace.openTextDocument(uri));
-    const root = postcss_less.parse(cssdoc.getText());
-    classNames = {};
-    compileCss((root?.nodes || []).filter((rule: any) => rule.type === 'rule'));
-    const index = (classNames['.' + arr[1]] || 1) - 1;
-    const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
-    vscode.window.showTextDocument(cssdoc, {
-      selection: range,
-    });
-  });
 
   let definition = vscode.languages.registerDefinitionProvider([
     { language: 'javascript', scheme: 'file' },
@@ -112,20 +64,34 @@ export function activate(context: vscode.ExtensionContext) {
     async provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
       const name = document.getText(document.getWordRangeAtPosition(position));
       const text = document.getText();
-      const stylesName = compileStylesName(text, name, position.line + 1);
-      const requirePath = compileRequirePath(text, stylesName);
+      const arrText = text.split('\n');
+      const lineText = arrText[position.line];
+      let stylesName = '';
+      lineText.replace(new RegExp("[a-zA-Z0-9_]{1,}." + name, "g"), (s, i) => {
+        if (!/[0-9]|[a-z]|[A-Z]|_/.test(lineText[i + s.length])) {
+          stylesName = s.split('.')[0];
+        }
+        return s;
+      });
+      const requirePaths = arrText.find(path => path.includes(' ' + stylesName + ' '));
+      let requirePath = '';
+      if (requirePaths?.includes("'")) {
+        requirePath = requirePaths.split("'")[1];
+      } else if (requirePaths?.includes('"')) {
+        requirePath = requirePaths.split('"')[1];
+      }
+      if (!requirePath) { return; }
       const uri = vscode.Uri.file(path.resolve(document.fileName, '../' + requirePath));
       const cssdoc = (await vscode.workspace.openTextDocument(uri));
       const root = postcss_less.parse(cssdoc?.getText());
-      classNames = {};
+      classNames = '.' + name;
+      lines = 0;
       compileCss((root?.nodes || []).filter((rule: any) => rule.type === 'rule'));
-      const index = (classNames['.' + name] || 1) - 1;
-      const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
+      const range = new vscode.Range(new vscode.Position(lines, 0), new vscode.Position(lines + 1, 0));
       return new vscode.Location((uri), range);
     }
   });
 
-  context.subscriptions.push(disposable_toCss);
   context.subscriptions.push(definition);
 }
 
